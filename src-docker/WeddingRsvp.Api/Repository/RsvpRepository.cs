684148a0705b093ea4438e02ba6e9abd402f1f7d
ludwig <blush-chewy-strike@duck.com>
2025-11-23 12:57:46 +0100

    #6 implement update rsvp

--- src-docker/WeddingRsvp.Api/Repository/RsvpRepository.cs
+++ src-docker/WeddingRsvp.Api/Repository/RsvpRepository.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using MongoDB.Driver;
 using WeddingRsvp.Api.Repository.Entities;
 using WeddingRsvp.Api.Repository.Generic;
@@ -6,12 +7,47 @@ namespace WeddingRsvp.Api.Repository;
 
 public class RsvpRepository : MongoDbRepository<Rsvp>, IRsvpRepository
 {
-    public RsvpRepository( IMongoClient mongoClient, ILogger<MongoDbRepository<Rsvp>> logger ) : base( mongoClient, logger )
+    public RsvpRepository(IMongoClient mongoClient, ILogger<MongoDbRepository<Rsvp>> logger) : base(mongoClient, logger)
     {
     }
 
-    public override Task<RepositoryResponse<Rsvp, RepositoryFailResponse>> UpdateAsync(Rsvp entity, CancellationToken cancellationToken = default)
+    public override async Task<RepositoryResponse<Rsvp, RepositoryFailResponse>> UpdateAsync(Rsvp entity,
+        CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (Collection is null)
+            return NotConnectedFailedResponse();
+
+        var filter = Builders<Rsvp>.Filter
+            .Eq(r => r.Id, entity.Id);
+
+        var update = Builders<Rsvp>.Update
+            .Set(r => r.NumberOfGuests, entity.NumberOfGuests)
+            .Set(r => r.NumberOfNormalMeals, entity.NumberOfNormalMeals)
+            .Set(r => r.NumberOfVeganMeals, entity.NumberOfVeganMeals)
+            .Set(r => r.NumberOfVegetarianMeals, entity.NumberOfVegetarianMeals)
+            .Set(r => r.AdditionalInformation, entity.AdditionalInformation);
+
+        try
+        {
+            var updated = await Collection.FindOneAndUpdateAsync<Rsvp>(filter, update, null, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (updated is not null)
+                return RepositoryResponse<Rsvp, RepositoryFailResponse>.CreateSuccess(updated);
+
+            RepositoryFailResponse fail = new()
+                { StatusCode = HttpStatusCode.NotFound, Message = "Document cannot be updated. Document not found." };
+            return RepositoryResponse<Rsvp, RepositoryFailResponse>.CreateFail(fail);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogCritical(ex, "Error reading from mongo.");
+            RepositoryFailResponse fail = new()
+            {
+                StatusCode = HttpStatusCode.InternalServerError,
+                Message = ex.Message,
+            };
+            return RepositoryResponse<Rsvp, RepositoryFailResponse>.CreateFail(fail);
+        }
     }
 }
