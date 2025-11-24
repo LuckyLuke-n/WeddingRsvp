a0aada206bced6923f6c36a30f8c3d03371366de
ludwig <blush-chewy-strike@duck.com>
2025-11-24 22:49:28 +0100

    #12 add validation

--- src-dotnet/WeddingRsvp.Abstractions/Models/PutRsvpDto.cs
+++ src-dotnet/WeddingRsvp.Abstractions/Models/PutRsvpDto.cs
@@ -1,14 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WeddingRsvp.Abstractions.Models;
 
 public class PutRsvpDto
 {
+    [Length(1, 255)]
     public string Name { get; set; } = string.Empty;
+    
+    [EnumDataType(typeof(GuestType))]
     public GuestType Type { get; set; }
+    
+    [Range(1, 10)]
     public int NumberOfGuests { get; set; }
     
+    [Required]
+    [Range(1, 10)]
     public int NumberOfGuestsAttending { get; set; }
+    
+    [Required]
+    [Range(1, 10)]
     public int NumberOfNormalMeals { get; set; }
+    
+    [Required]
+    [Range(1, 10)]
     public int NumberOfVegetarianMeals { get; set; }
+    
+    [Required]
+    [Range(1, 10)]
     public int NumberOfVeganMeals { get; set; }
     
     public string AdditionalInformation { get; set; } = string.Empty;
