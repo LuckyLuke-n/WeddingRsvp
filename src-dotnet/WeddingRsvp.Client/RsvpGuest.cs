1ce77b74d06526876393d43854690e418aca926d
ludwig <blush-chewy-strike@duck.com>
2025-12-08 22:52:01 +0100

    #3 add rsvp page

---
+++ src-dotnet/WeddingRsvp.Client/RsvpGuest.cs
@@ -0,0 +1,16 @@
+﻿using WeddingRsvp.Abstractions.Models;
+
+namespace WeddingRsvp.Client;
+
+public class RsvpGuest
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public ResponseType Response { get; set; }
+    public int NumberOfGuestsAttending { get; set; }
+    public int NumberOfGuestsOvernight { get; set; }
+    public int NumberOfNormalMeals { get; set; }
+    public int NumberOfVegetarianMeals { get; set; }
+    public int NumberOfVeganMeals { get; set; }
+    public string AdditionalInformation { get; set; } = string.Empty;
+}
